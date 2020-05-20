727685be18c8dcb2c07939f65fd91ec52ba93fa2
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-05-20 18:58:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,13 +30,7 @@ class Song
   end
   
   def self.find_by_name(name)
-    songs  = []
-    @@all.each do |song|
-      if song.name == name 
-        songs.push(name)
-      end
-    end
-    return songs
+    @all.find
   end
 
 end
